832e7c8e64c366cbd93807ceda4031af90917ef7
ny <71589865+Yigit-Findik@users.noreply.github.com>
2026-04-19 20:42:12 +0200

    SCRUM-55 has been done
    Update: added qol_score to migration and seeders for city_functions.

--- database/migrations/2026_04_15_110038_create_city_functions_table.php
+++ database/migrations/2026_04_15_110038_create_city_functions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('city_functions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('qol_score');
             $table->string('category');
             $table->string('image_path');
             $table->text('description')->nullable();
