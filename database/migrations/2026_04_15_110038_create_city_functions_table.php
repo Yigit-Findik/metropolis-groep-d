c0054eec71e3ce095a915b4d1f4e467f2b4ae487
Jurre785 <jurrevancuijk@gmail.com>
2026-04-23 20:21:43 +0200

    Added the correct functions
    Added the functions that Anne wanted

--- database/migrations/2026_04_15_110038_create_city_functions_table.php
+++ database/migrations/2026_04_15_110038_create_city_functions_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('city_functions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('qol_score');
             $table->string('category');
             $table->string('image_path');
             $table->text('description')->nullable();
