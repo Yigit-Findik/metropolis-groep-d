1dd0514ee561fa85d5a45ff86841910a2e178372
lucashamers2008 <lucashamers2008@gmail.com>
2026-06-20 11:16:23 +0200

    now there is no href
    now it is just a picture

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,10 +25,8 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 hc:bg-black dark:bg-gray-900">
-            <div>
-                <a href="/" class="flex items-center justify-center bg-white rounded-lg px-3 py-2">
-                    <x-application-logo class="w-20 h-20" />
-                </a>
+            <div class="flex items-center justify-center bg-white rounded-lg px-3 py-2">
+                <x-application-logo class="w-20 h-20" />
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white hc:bg-black hc:border hc:border-white dark:bg-gray-800 shadow-md hc:shadow-none overflow-hidden sm:rounded-lg">
